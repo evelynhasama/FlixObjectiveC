a3aadddea36289b0d180e8c3eaa56d4d2540ae3a
Evelyn Hasama <ehasama11@gmail.com>
2022-01-24 10:17:59 -0800

    Pull to refresh and segue to movie details

---
+++ FlixObjectiveC/ViewControllers/DetailsViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailsViewController.h
+//  FlixObjectiveC
+//
+//  Created by Evelyn Hasama on 1/24/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailsViewController : UIViewController
+
+@property (nonatomic, strong) NSDictionary *movie;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FlixObjectiveC/Views/MovieCell.m
@@ -0,0 +1,23 @@
+//
+//  MovieCell.m
+//  FlixObjectiveC
+//
+//  Created by Evelyn Hasama on 1/23/22.
+//
+
+#import "MovieCell.h"
+
+@implementation MovieCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
